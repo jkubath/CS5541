2f02ffc8f81db80f2d3c35fbc20323fc29661ef1
Jonah Kubath <jkubath1@gmail.com>
2018-01-10 14:34:12 -0500

    Hello.c

---
+++ Hello.c
@@ -0,0 +1,13 @@
+/*
+  Name: Jonah Kubath
+  Class: CS5541
+  Summary: Basic C program to test if the connection to the server is good
+
+*/
+
+#include <stdio.h>
+
+int main(void) {
+  printf("Jonah Kubath\n");
+  printf("Hello World\n");
+}
